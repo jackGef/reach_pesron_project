6caf01def8004c3cfd851085022d58be1f83c86d
miro972 <matanmiller1610@gmail.com>
2025-02-27 19:02:26 +0200

    updated

--- Python/main.py
+++ Python/main.py
@@ -1,37 +1,44 @@
+import math
+import sys
 import pandas as pd
 import numpy as np
-
-df = pd.read_csv("C:/Users/Matan/Downloads/Score.csv/Score.csv")
-Y = (df[['Credit_Score']])
-df = df.drop(columns=['Payment_of_Min_Amount', 'Credit_Mix', 'Payment_Behaviour', 'Credit_Score'])
-
-Y = np.where(Y != "Poor", Y, 0)
-Y = np.where(Y != "Standard", Y, 0.33333)
-Y = np.where(Y != "Good", Y, 0.66666)
-
-B = 0
-# pd.set_option('display.max_columns', None)
-W = np.random.randn(17, 1) * 0.01
-X = np.matrix(df[:8000]).T
-print("X first index is ", X[0])
-print(X.mean())
-Z = np.dot(W.T, X) + B
-print(Z, "\n")
-print("W:", W, W.shape, "end\n\n", "X:", X, X.shape, "end\n\n", "B:", B, "end\n\n")
 
 
 def sigmoid(Z_func):
     return 1 / (1 + (np.exp(-Z_func)))
 
 
+# receiving data
+df = pd.read_csv("C:/Users/Matan/Downloads/Score.csv/Score.csv")
+Y = (df[['Credit_Score']])
+df = df.drop(columns=['Payment_of_Min_Amount', 'Credit_Mix', 'Payment_Behaviour', 'Credit_Score'])
+np.set_printoptions(threshold=sys.maxsize)
+
+# normalization
+Y = np.where(Y != "Poor", Y, 0)
+Y = np.where(Y != "Standard", Y, 0.33333)
+Y = np.where(Y != "Good", Y, 0.66666)
+
+# Vector creation and initialization
+B = 0
+W = np.random.randn(17, 1) * 0.01
+X = np.matrix(df.iloc[:8000]).T
+x = pd.DataFrame(X.T)
+Z = np.dot(W.T, x.T) + B
+
+print("the data is: \n", x)
+
+# forward propagation
 A = sigmoid(Z)
-print("A matrix is:", A)
-print(np.shape(A))
-print("hi")
-
+print("Z vector is : \n", Z)
+print("A vector is : \n", A)
+print(type(Z))
 dZ = A - Y
-print("dZ is",dZ)
-
+# print("A is", A)
+# print("Y is", Y)
+print("dZ type is ", type(dZ))
+print("dZ is", dZ)
+print("END")
 
 # def data_collect():
 #     df = pd.read_csv("C:/Users/Matan/Downloads/Score.csv/Score.csv")
@@ -47,5 +54,5 @@ print("dZ is",dZ)
 #     W = np.random.randn(17, 1)
 #     X = np.matrix(df[:8000]).T
 #     Z = np.dot(W.T, X) + B
-
-
+# print(Z, "\n")
+# print("W:", W, W.shape, "end\n\n", "X:", X, X.shape, "end\n\n", "B:", B, "end\n\n")
