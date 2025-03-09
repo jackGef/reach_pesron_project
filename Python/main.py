01efd0db1bccddd27bba5fb3c57bb4220d25e436
miro972 <matanmiller1610@gmail.com>
2025-03-09 08:58:55 +0200

    new project structure

--- Python/main.py
+++ Python/main.py
@@ -1,58 +1,24 @@
-import math
-import sys
 import pandas as pd
-import numpy as np
 
 
-def sigmoid(Z_func):
-    return 1 / (1 + (np.exp(-Z_func)))
+def recieving_data():
+    return
 
 
-# receiving data
-df = pd.read_csv("C:/Users/Matan/Downloads/Score.csv/Score.csv")
-Y = (df[['Credit_Score']])
-df = df.drop(columns=['Payment_of_Min_Amount', 'Credit_Mix', 'Payment_Behaviour', 'Credit_Score'])
-np.set_printoptions(threshold=sys.maxsize)
+def trim_data(X):
+    # return X_train,Y_train,X_val,Y_val,X_test,Y_test
 
-# normalization
-Y = np.where(Y != "Poor", Y, 0)
-Y = np.where(Y != "Standard", Y, 0.33333)
-Y = np.where(Y != "Good", Y, 0.66666)
+def initialize_parameters():
+    #  Create W and B
+def linear_computation(X_train,):
+    #return Z
 
-# Vector creation and initialization
-B = 0
-W = np.random.randn(17, 1) * 0.01
-X = np.matrix(df.iloc[:8000]).T
-x = pd.DataFrame(X.T)
-Z = np.dot(W.T, x.T) + B
+def sigmoid(Z):
+    # return A
 
-print("the data is: \n", x)
-
-# forward propagation
-A = sigmoid(Z)
-print("Z vector is : \n", Z)
-print("A vector is : \n", A)
-print(type(Z))
-dZ = A - Y
-# print("A is", A)
-# print("Y is", Y)
-print("dZ type is ", type(dZ))
-print("dZ is", dZ)
-print("END")
-
-# def data_collect():
-#     df = pd.read_csv("C:/Users/Matan/Downloads/Score.csv/Score.csv")
-#
-# def normalization():
-#     df = df.drop(columns=['Payment_of_Min_Amount', 'Credit_Mix', 'Payment_Behaviour', 'Credit_Score'])
-#     Y = np.where(Y != "Poor", Y, 0)
-#     Y = np.where(Y != "Standard", Y, 0.33333)
-#     Y = np.where(Y != "Good", Y, 0.66666)
-#
-# def initialization():
-#     B = 0
-#     W = np.random.randn(17, 1)
-#     X = np.matrix(df[:8000]).T
-#     Z = np.dot(W.T, X) + B
-# print(Z, "\n")
-# print("W:", W, W.shape, "end\n\n", "X:", X, X.shape, "end\n\n", "B:", B, "end\n\n")
+def cost(A,Y):
+    #return cost
+def backward_propagation(Z,A):
+    #return dW,dB
+def update_paramaters(W,B,dW,dB):
+    #return W,B
